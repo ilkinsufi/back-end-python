583982043f89b0d0887626e49e772b97a6d192f2
ilkinsufi <ilkinsufi@gmail.com>
2024-02-08 22:00:47 +0400

    Variables

--- app..py
+++ app..py
@@ -1,1 +1,8 @@
-print("Hellow World!") 
+# print("Hellow World!") 
+# print("Welcome to the world of python", 100)
+# print("Welcome to the world of python",100)
+# print("Welcome to the world of python:",100)
+# print("Welcome to the world of python:",100)
+# name = "Ilkin"
+# print("Hello " + name)
+# print("Hello" + " " + name)
