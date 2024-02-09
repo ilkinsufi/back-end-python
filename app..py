0d4719618958ae4b6d9452d3b1da4aa58cda2509
ilkinsufi <ilkinsufi@gmail.com>
2024-02-09 19:14:14 +0400

    Word Replacement Exercise

--- app..py
+++ app..py
@@ -19,6 +19,17 @@
 # print(min([1, 2, 3, 4, 5, 6, 7, 8, 9, -10]))
 # print(max([1, 2, 3, 4, 5, 6, 7, 8, 9, -10]))
 # print(round(10.1, 999, ))
-from math import *
+# from math import *
+# print(sqrt(100))
+# a = input("adini yaz")
+# print("senin adin budur: ",a)
+# cumle = input("cumlenizi daxil edin")
+# print("sizin cumleniz: ", cumle)
+# soz = input("hansi sozu deyisdirmek isteyirsiniz")
+# soz2= input("hansi soz ile deyisdirek?")
+# print(cumle.replace(soz,soz2))
 
-print(bin(1))
+
+
+
+
